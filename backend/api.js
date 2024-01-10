667601f509ff13b15147bc930948c522bec7d27e
Charles Ngo <charlesngo37@gmail.com>
2024-01-09 17:45:14 -0800

    added another sample

--- backend/api.js
+++ backend/api.js
@@ -1,12 +1,18 @@
 const mtg = require("mtgsdk");
 
-mtg.card.find(3).then((result) => {
-  console.log(result.card.name); // "Black Lotus"
-});
+// Code will print black lotus
+// mtg.card.find(3).then((result) => {
+//   console.log(result.card.name); // "Black Lotus"
+// });
 
 // The code above works but the code below doesn't. Next steps are to figure out why.
 mtg.card
-  .all({ types: "instant", colors: "red,white", set: "LCI" })
-  .on("data", function (card) {
+  .all({ type: "instant", colors: "red", set: "LCI" })
+  .on("data", (card) => {
     console.log(card.name);
   });
+
+// // This sample code does work and prints all instances of squee
+// mtg.card.all({ name: "Squee", pageSize: 1 }).on("data", (card) => {
+//   console.log(card.name);
+// });
