223f26cfa93186233bc75cfcdc3232c8964c4354
Charles Ngo <charlesngo37@gmail.com>
2024-01-13 13:50:10 -0800

    removed mtgsdk and implementing scryfall

--- backend/api.js
+++ backend/api.js
@@ -1,18 +1,2 @@
-const mtg = require("mtgsdk");
-
-// Code will print black lotus
-// mtg.card.find(3).then((result) => {
-//   console.log(result.card.name); // "Black Lotus"
-// });
-
-// The code above works but the code below doesn't. Next steps are to figure out why.
-mtg.card
-  .all({ type: "instant", colors: "red", set: "LCI" })
-  .on("data", (card) => {
-    console.log(card.name);
-  });
-
-// // This sample code does work and prints all instances of squee
-// mtg.card.all({ name: "Squee", pageSize: 1 }).on("data", (card) => {
-//   console.log(card.name);
-// });
+// magicthegathering.io MTGSDK API is currently out of date and does not return recent set information.
+// Transitioning to scryfall api as it also contains card images
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,10 @@
 const express = require("express");
-const mtg = require("mtgsdk");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Successful response.");
+  console.log("Server response");
 });
 
-app.use((req, res, next) => {
-  mtg.card
-    .all({ types: "instant", colors: "red,white", set: "LCI" })
-    .on("data", function (card) {
-      console.log(card.name);
-    });
-  next();
-});
-
-app.get("/card", (req, res) => {
-  console.log("Entering /card");
-  mtg.card.find(3).then((result) => {
-    console.log(result.card.name); // "Black Lotus"
-  });
-});
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
